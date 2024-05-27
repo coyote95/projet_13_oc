c7b4e56561b8160ab6e8821b85cdac9034542a64
coyote95 <marie.guillot.oc@gmail.com>
2024-05-27 11:27:57 +0200

    migrations voie courte

--- lettings/models.py
+++ lettings/models.py
@@ -14,13 +14,19 @@ class Address(models.Model):
     def __str__(self):
         return f'{self.number} {self.street}'
 
+    class Meta:
+        db_table = 'oc_lettings_site_address'
+
 
 class Letting(models.Model):
     title = models.CharField(max_length=256)
-    address = models.OneToOneField(Address, on_delete=models.CASCADE)
+    address = models.OneToOneField(Address, on_delete=models.CASCADE, related_name='+')
 
     def __str__(self):
         return self.title
+
+    class Meta:
+        db_table = 'oc_lettings_site_letting'
 
 
 
