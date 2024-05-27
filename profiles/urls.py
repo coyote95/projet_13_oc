c533a3e1ac759059bfd9f9d0947aec95302bf7b3
coyote95 <marie.guillot.oc@gmail.com>
2024-05-27 15:31:38 +0200

    templates views profiles rename index

--- profiles/urls.py
+++ profiles/urls.py
@@ -4,7 +4,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.profiles_index, name='profiles_index'),
+    path('', views.index, name='profiles_index'),
     path('<str:username>/', views.profile, name='profile'),
 
 ]
