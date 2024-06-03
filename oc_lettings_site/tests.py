ea6282796626d1e78263bffa9fee47ef60efd5d2
coyote95 <marie.guillot.oc@gmail.com>
2024-06-03 20:41:07 +0200

    add test oc_lettings_site

--- oc_lettings_site/tests.py
+++ oc_lettings_site/tests.py
@@ -1,2 +1,54 @@
+import pytest
+from django.test import Client
+from django.urls import reverse, resolve
+from pytest_django.asserts import assertTemplateUsed
+
+
 def test_dummy():
     assert 1
+
+
+def test_index_url():
+    path = reverse('index')
+    assert path == f"/"
+    assert resolve(path).view_name == "index"
+
+
+@pytest.mark.django_db
+def test_admin_url():
+    path = reverse('admin:index')
+    assert path == "/admin/"
+    assert resolve(path).url_name.split(":")[-1] == "index"
+
+
+@pytest.mark.django_db
+def test_index_view():
+    client = Client()
+    path = reverse('index')
+    response = client.get(path)
+    content = response.content.decode()
+    assert "<title>Holiday Homes</title>" in content
+    assert response.status_code == 200
+    assertTemplateUsed(response, "index.html")
+
+
+@pytest.mark.django_db
+def test_wrong_url():
+    client = Client()
+    response = client.get("/toto/")
+    assert response.status_code == 404
+    assertTemplateUsed(response, "404.html")
+
+
+@pytest.mark.django_db
+def test_integration():
+    client = Client()
+    home_url = reverse('index')
+    response = client.get(home_url)
+    assert response.status_code == 200
+    profiles_url = reverse('profiles_index')
+    response = client.get(profiles_url)
+    assert response.status_code == 200
+    letting_url = reverse('lettings_index')
+    response = client.get(letting_url)
+    assert response.status_code == 200
